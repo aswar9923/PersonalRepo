4861f4274d8807d2b552f4f9e7a1c478f017b0e3
pooja <aswarpooja98@gmail.com>
2025-02-12 13:02:23 +0530

    run

---
+++ Even_odd_no.java
@@ -0,0 +1,14 @@
+public class Even_odd_no{
+public static void main(String[] args){
+int a=10;
+
+if(a%2==0){
+System.out.println("even number");
+
+}
+else{
+System.out.println("odd number");
+}
+}
+
+}
