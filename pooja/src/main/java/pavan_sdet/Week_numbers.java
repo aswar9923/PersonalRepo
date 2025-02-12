991a2447f3183899bb6bb087881db3eddd9641c1
pooja <aswarpooja98@gmail.com>
2025-02-12 15:27:24 +0530

    project

---
+++ pooja/src/main/java/pavan_sdet/Week_numbers.java
@@ -0,0 +1,27 @@
+package pavan_sdet;
+
+public class Week_numbers {
+
+	public static void main(String[] args) {
+		String weekname="monday";
+		
+		switch(weekname) {
+		case "sunday":
+			System.out.println(1);
+			break;
+		case "monday":
+			System.out.println(2);
+			break;
+		case "tusday":
+			System.out.println(3);
+			break;
+		case "wednessday":
+			System.out.println(4);
+			break;
+			default:
+				System.out.println(8);
+		}
+
+	}
+
+}
